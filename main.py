06983d434109a42e146cfd57ac67fca2443de001
Moises Angeles <moises.ea.camilo@gmail.com>
2025-01-29 18:55:08 -0500

    added forward movement

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ from constants import *
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x,y)    
-    
+    dt = 0
 
     while True:
-        dt = clock.tick(60) / 1000
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -22,7 +20,8 @@ def main():
         player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
+        
      
        
 if __name__ == "__main__":
