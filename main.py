521c2929b6ed7533ff828fcf3f04e5a6451a9fe9
Moises Angeles <moises.ea.camilo@gmail.com>
2025-01-29 17:10:23 -0500

    added player.py, player draw, player rotation and rotation speed

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pygame
+from player import *
 from constants import * 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x,y)    
     
+
     while True:
-       screen.fill((0,0,0))
-       pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+
+     
        
-       for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-               return
-   
 if __name__ == "__main__":
     main()
