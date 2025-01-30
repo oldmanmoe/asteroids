af757eba9917207d0315e8238c49480f82828df6
Moises Angeles <moises.ea.camilo@gmail.com>
2025-01-29 19:41:37 -0500

    learning to use groups in pygame

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x,y)    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     dt = 0
 
+    updatable.add(player)
+    drawable.add(player)
     while True:
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        
+        for drawing in drawable:
+            drawing.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
